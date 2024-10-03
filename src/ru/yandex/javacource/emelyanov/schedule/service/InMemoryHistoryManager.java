811f7209263d49fa24f67f6118b95c089bedc7ba
StanislavVE77 <s.emelyanov@complex-solutions.ru>
2024-10-03 13:32:20 +0300

    V.5 (Спринт 6)

--- src/ru/yandex/javacource/emelyanov/schedule/service/InMemoryHistoryManager.java
+++ src/ru/yandex/javacource/emelyanov/schedule/service/InMemoryHistoryManager.java
@@ -2,31 +2,84 @@ package ru.yandex.javacource.emelyanov.schedule.service;
 
 import ru.yandex.javacource.emelyanov.schedule.model.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    static final int SIZE_OF_HISTORY = 10;
-    private List<Task> history = new ArrayList<>(SIZE_OF_HISTORY);
+
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
+
+        Node(Node prev, Task item, Node next) {
+            this.next = next;
+            this.prev = prev;
+            this.task = item;
+        }
+    }
+
+
+    private Map<Integer, Node> history = new HashMap<>();
+    Node first;
+    Node last;
 
     public InMemoryHistoryManager() {
         this.history = history;
     }
 
     @Override
-    public List<Task> getHistory() {
-        return history;
+    public List<Task> getAll() {
+        return getTasks();
     }
 
     @Override
     public void add(Task task) {
-        //Task newTask = new Task(task.getName(), task.getStatus(),task.getDescription());
-        if (task == null) {
+        Node node = history.get(task.getId());
+        if (node != null) {
+            removeNode(node);
+        }
+        linkLast(task);
+        history.put(task.getId(), last);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node node = history.get(id);
+        removeNode(node);
+    }
+
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        for (Node node = first; node != null; node = node.next) {
+            tasks.add(node.task);
+        }
+        return tasks;
+    }
+
+    void linkLast(Task task) {
+        final Node node = last;
+        final Node newNode = new Node(node, task, null);
+        last = newNode;
+        if (node == null)
+            first = newNode;
+        else
+            node.next = newNode;
+    }
+
+    private void removeNode(Node node) {
+        if (node == null) {
             return;
         }
-        if (history.size() >= SIZE_OF_HISTORY) {
-            history.removeFirst();
+        if (node.next == null) {
+            last = node.prev;
+        } else {
+            node.next.prev = node.prev;
         }
-        history.add(task);
+        if (node.prev == null) {
+            first = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        history.remove(node.task.getId());
     }
 }
